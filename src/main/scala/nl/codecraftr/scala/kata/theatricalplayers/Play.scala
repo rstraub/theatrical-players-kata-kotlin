91268413c367617d41d93ba92eaa5a424f1cf848
Roy Straub <roy@codecraftr.nl>
2024-02-28 12:38:07 +0100

    Simpler cost calculation

--- src/main/scala/nl/codecraftr/scala/kata/theatricalplayers/Play.scala
+++ src/main/scala/nl/codecraftr/scala/kata/theatricalplayers/Play.scala
@@ -1,23 +1,22 @@
 package nl.codecraftr.scala.kata.theatricalplayers
 
+// TODO this can become a hierarchy and polymorphic
 final case class Play(name: String, `type`: String) {
   def calculateCosts(audience: Int): Int = {
-    var costs = 0
     `type` match {
-      case "tragedy" => {
-        costs = 40000
+      case "tragedy" =>
+        var costs = 40000
         if (audience > 30)
           costs += 1000 * (audience - 30)
-      }
-      case "comedy" => {
-        costs = 30000
+        costs
+      case "comedy" =>
+        var costs = 30000
         if (audience > 20)
           costs += 10000 + 500 * (audience - 20)
         costs += 300 * audience
-      }
+        costs
       case _ => throw new Exception("unknown type: " + `type`)
     }
-    costs
   }
 
   def calculateCredits(audience: Int): Int = {
