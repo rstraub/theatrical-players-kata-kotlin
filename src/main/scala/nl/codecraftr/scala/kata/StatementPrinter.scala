e992037a845bed59e001a7d1a12c53250e6f8d37
Roy Straub <roy@codecraftr.nl>
2024-02-28 11:57:42 +0100

    Extracted creation of statement lines

--- src/main/scala/nl/codecraftr/scala/kata/StatementPrinter.scala
+++ src/main/scala/nl/codecraftr/scala/kata/StatementPrinter.scala
@@ -12,25 +12,44 @@ class StatementPrinter {
   private val culture = Locale.US
 
   def print(invoice: Invoice, plays: Map[String, Play]): String = {
-    var result = createHeader(invoice)
-
     var totalAmount = 0
     for (perf <- invoice.performances) {
       val play = plays(perf.playId)
       val performanceCost = calculatePerformanceCost(play, perf)
       totalAmount += performanceCost
-
-      // print line for this order
-      result += s"  ${play.name}: ${NumberFormat
-          .getCurrencyInstance(culture)
-          .format((performanceCost / 100).toDouble)} (${perf.audience} seats)$lineSeparator"
     }
 
     val totalCredits = calculateTotalCredits(invoice, plays)
 
+    var result = createHeader(invoice)
+    result += createLines(invoice, plays)
     result += createFooter(totalAmount, totalCredits)
 
     result
+  }
+
+  private def createHeader(invoice: Invoice) =
+    s"Statement for ${invoice.customer}$lineSeparator"
+
+  private def createLines(invoice: Invoice, plays: Map[String, Play]) = {
+    var lines = ""
+    for (perf <- invoice.performances) {
+      val play = plays(perf.playId)
+      val performanceCost = calculatePerformanceCost(play, perf)
+
+      // print line for this order
+      lines += s"  ${play.name}: ${NumberFormat
+          .getCurrencyInstance(culture)
+          .format((performanceCost / 100).toDouble)} (${perf.audience} seats)$lineSeparator"
+    }
+    lines
+  }
+
+  private def createFooter(totalAmount: Int, volumeCredits: Int) = {
+    val line1 =
+      s"Amount owed is ${NumberFormat.getCurrencyInstance(culture).format(totalAmount / 100d)}$lineSeparator"
+    val line2 = s"You earned ${volumeCredits} credits$lineSeparator"
+    line1 + line2
   }
 
   private def calculatePerformanceCost(play: Play, perf: Performance) = {
@@ -68,15 +87,5 @@ class StatementPrinter {
         totalCredits += Math.floor(perf.audience / 5d).toInt
     }
     totalCredits
-  }
-
-  private def createHeader(invoice: Invoice) =
-    s"Statement for ${invoice.customer}$lineSeparator"
-
-  private def createFooter(totalAmount: Int, volumeCredits: Int) = {
-    val line1 =
-      s"Amount owed is ${NumberFormat.getCurrencyInstance(culture).format(totalAmount / 100d)}$lineSeparator"
-    val line2 = s"You earned ${volumeCredits} credits$lineSeparator"
-    line1 + line2
   }
 }
