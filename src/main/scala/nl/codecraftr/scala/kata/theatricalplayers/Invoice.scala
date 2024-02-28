d51f50760fdc67d9e58889958459e68d5c7b4d50
Roy Straub <roy@codecraftr.nl>
2024-02-28 12:30:59 +0100

    Accumulate costs using map

--- src/main/scala/nl/codecraftr/scala/kata/theatricalplayers/Invoice.scala
+++ src/main/scala/nl/codecraftr/scala/kata/theatricalplayers/Invoice.scala
@@ -3,15 +3,10 @@ package nl.codecraftr.scala.kata.theatricalplayers
 final case class Invoice(customer: String, performances: List[Performance]) {
   def calculateCosts(
       plays: Map[String, Play]
-  ): Int = {
-    var totalAmount = 0
-    for (perf <- performances) {
-      val play = plays(perf.playId)
-      val performanceCost = play.calculateCosts(perf.audience)
-      totalAmount += performanceCost
-    }
-    totalAmount
-  }
+  ): Int =
+    performances
+      .map(perf => plays(perf.playId).calculateCosts(perf.audience))
+      .sum
 
   def calculateCredits(
       plays: Map[String, Play]
