86a69f889bbc5623025672afbf63f91df17106c1
Roy Straub <roy@codecraftr.nl>
2024-03-30 20:05:45 +0100

    Made plays a hierarchy

--- src/main/scala/nl/codecraftr/scala/kata/theatricalplayers/Play.scala
+++ src/main/scala/nl/codecraftr/scala/kata/theatricalplayers/Play.scala
@@ -1,7 +1,7 @@
 package nl.codecraftr.scala.kata.theatricalplayers
 
 // TODO this can become a hierarchy and polymorphic
-final case class Play(name: String, `type`: String) {
+case class Play(name: String, `type`: String) {
   def calculateCosts(audience: Int): Int = {
     `type` match {
       case "tragedy" =>
@@ -26,6 +26,42 @@ final case class Play(name: String, `type`: String) {
     // add extra credit for every ten comedy attendees
     if ("comedy" == `type`)
       playCredits += Math.floor(audience / 5d).toInt
+    playCredits
+  }
+}
+
+sealed trait PlayX {
+  def calculateCosts(audience: Int): Int = {
+    var costs = 40000
+    if (audience > 30)
+      costs += 1000 * (audience - 30)
+    costs
+  }
+
+  def calculateCredits(audience: Int): Int = {
+    var playCredits = 0
+    playCredits += Math.max(audience - 30, 0)
+    playCredits
+  }
+}
+
+final case class Tragedy(name: String) extends PlayX
+
+final case class Comedy(name: String) extends PlayX {
+  override def calculateCosts(audience: Int): Int = {
+    var costs = 30000
+    if (audience > 20)
+      costs += 10000 + 500 * (audience - 20)
+    costs += 300 * audience
+    costs
+  }
+
+  override def calculateCredits(audience: Int): Int = {
+    var playCredits = 0
+    // Add credits for every attendee above 30
+    playCredits += Math.max(audience - 30, 0)
+    // add extra credit for every ten comedy attendees
+    playCredits += Math.floor(audience / 5d).toInt
     playCredits
   }
 }
